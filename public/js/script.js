f1df8743d048db3b5af1318230207c1c6176408d
Sparsh Nayak <sparshn26@gmail.com>
2025-04-25 23:16:34 -0700

    Moved files and relinked index.html
    images weren't showing on my computer

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+let isMenuOpen = false;
+
+const menuButton = document.querySelector(".menu-btn");
+
+menuButton.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
+    if(isMenuOpen){
+        menuButton.classList.add('open')
+    }
+    else{
+        menuButton.classList.remove('open')
+    }
+})
