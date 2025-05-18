3a84864201016a41944948913e0f4bb861991b83
jdelacruz23 <924460027@sfsu.edu>
2025-05-18 01:08:02 -0700

    refer to .txt

--- app.js
+++ app.js
@@ -94,6 +94,14 @@ app.get('/product-page', (req, res) => {
   });
 });
 
+app.get('/store.html', (req, res) => {
+  res.render('store', { title: 'Store' });
+});
+
+app.get('/store', (req, res) => {
+  res.render('store', { title: 'Store' });
+});
+
 app.get('/product-carousel', (req, res) => {
   const imageUrls = [
     "https://images.unsplash.com/photo-1605773527852-c546a8584ea3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
